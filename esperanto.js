9bf660fb792a6c7844e3a6508e6792f5979c6984
Anders Janmyr <anders.janmyr@factor10.com>
2009-03-04 08:58:55 +0100

    added solution

--- esperanto.js
+++ esperanto.js
@@ -14,11 +14,24 @@ Esperanto.Lab = function() {
 		
 		_example: function() {
 			return "_example";
-		}
+		},
 				
 		// mixin: function(mixee, mixin) {}		
+		mixin: function(mixee, mixin) {
+			for (var name in mixin) {
+			    if (name.charAt(0) != '_') 
+			    	(mixee[name] = mixin[name]); 
+			}
+		},
 		
 		// map: function(conversionFunction) {}
+		map: function(conversionFunction) {
+			var newArr = [];
+			for (var i=0; i < this.length; i++) {
+				newArr.push(conversionFunction(this[i]));
+			};
+			return newArr;
+		}
 		
 	}
 	
@@ -26,7 +39,19 @@ Esperanto.Lab = function() {
 
 
 //Array.prototype.remove = function(item) {}
+Array.prototype.remove = function(item) {
+    for (i = 0; i < this.length; i++) {
+        if (item == this[i])  this.splice(i, 1);
+    }
+	return this;
+}
 
 
 //Function.prototype.kurry = function() {}
-
+Function.prototype.kurry = function() {
+    var fn = this;
+    var args = Array.slice(arguments, 0);
+    return function() {
+        return fn.apply(this, args.concat(Array.slice(arguments, 0)));
+    };
+}
