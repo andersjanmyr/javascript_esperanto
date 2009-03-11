40cc7e1af8e73d5d7dfc4c11c5d37f7af5417f19
Anders Janmyr <anders.janmyr@factor10.com>
2009-03-11 10:45:00 +0100

    removed solutions

--- esperanto.js
+++ esperanto.js
@@ -14,24 +14,11 @@ Esperanto.Lab = function() {
 		
 		_example: function() {
 			return "_example";
-		},
+		}
 				
 		// mixin: function(mixee, mixin) {}		
-		mixin: function(mixee, mixin) {
-			for (var name in mixin) {
-			    if (name.charAt(0) != '_') 
-			    	(mixee[name] = mixin[name]); 
-			}
-		},
 		
 		// convert: function(conversionFunction) {}
-		convert: function(conversionFunction) {
-			var newArr = [];
-			for (var i=0; i < this.length; i++) {
-				newArr.push(conversionFunction(this[i]));
-			};
-			return newArr;
-		}
 		
 	}
 	
@@ -39,19 +26,7 @@ Esperanto.Lab = function() {
 
 
 //Array.prototype.remove = function(item) {}
-Array.prototype.remove = function(item) {
-    for (i = 0; i < this.length; i++) {
-        if (item == this[i])  this.splice(i, 1);
-    }
-	return this;
-}
 
 
 //Function.prototype.kurry = function() {}
-Function.prototype.kurry = function() {
-    var fn = this;
-    var args = Array.slice(arguments, 0);
-    return function() {
-        return fn.apply(this, args.concat(Array.slice(arguments, 0)));
-    };
-}
+
