a02ca26f91a355e305e6329b368ea87e5b250214
Anders Janmyr <anders@janmyr.com>
2010-11-03 10:40:37 +0100

    Updated to new version of code that is simple to understand.

--- esperanto.js
+++ esperanto.js
@@ -2,31 +2,31 @@
 var Esperanto = Esperanto || {};
 
 Esperanto.Lab = function() {
-	var privVar = "example";
-	function privFunc() {
-		return privVar;
-	}
-	
-	return {
-		example: function() {
-			return privFunc();
-		},
-		
-		_example: function() {
-			return "_example";
-		} // Add a comma after the brace when removing the comment below.
-						
-		// mixin: function(mixee, mixin) {}		
-		
-		// convert: function(conversionFunction) {}
-		
-	}
-	
+  var privVar = "example";
+  function privFunc() {
+    return privVar;
+  }
+
+  return {
+    example: function() {
+      return privFunc();
+    },
+
+    _example: function() {
+      return "_example";
+    } // Add a comma after the brace when removing the comment below.
+
+    // 2. mixin: function(mixee, mixin) {}
+
+  }
+
 }()
 
+//1. Array.prototype.remove = function(item) {}
 
-//Array.prototype.remove = function(item) {}
+
+//3. Array.prototype.convert: function(conversionFunction) {}
 
 
-//Function.prototype.kurry = function() {}
+//4. Function.prototype.curry = function() {}
 
